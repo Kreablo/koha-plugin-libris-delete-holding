af5d22f2c59ec902c4e4211a415bdba832e8cfad
Andreas Jonsson <andreas.jonsson@kreablo.se>
2025-07-04 12:21:09 +0200

    Prepare version 1.6

--- src/Koha/Plugin/LibrisDeleteHolding.pm
+++ src/Koha/Plugin/LibrisDeleteHolding.pm
@@ -15,13 +15,13 @@ require YAML::XS;
 use strict;
 use warnings;
 
-our $VERSION = "1.5";
+our $VERSION = "1.6";
 
 our $metadata = {
     name            => 'Libris Delete Holding Module',
     author          => 'Andreas Jonsson',
     date_authored   => '2023-12-15',
-    date_updated    => "2025-05-22",
+    date_updated    => "2025-07-04",
     minimum_version => 22.11,
     maximum_version => '',
     version         => $VERSION,
